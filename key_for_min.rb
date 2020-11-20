7294303616065b26bf823c7b098dcb95980fab02
Eric <eric.m.lombardo@gmail.com>
2020-11-19 19:57:16 -0500

    commit tree 34a8d3c7e71dc231565400035b8dce3e34c5ce74

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,16 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   nil if name_hash.length == 0    #=> nil if hash is empty
-  winner = {}   # hash for placeholder for lowest
+  winner = {}   # empty hash for temporary lowest
   name_hash.each do |name, number|    # loop through each item in the hash
-    if winner.length == 0   # if first time through, fill the placeholder with first entry passed
+    if winner.length == 0   # if 1st time through, fill temp. lowest with key and value
       winner[:name] = name
       winner[:number] = number 
-    elsif winner[:number] > number    # if != first pass, check if new number is lower, if true replace winner hash
+    # if != first pass, check if new number is lower, if true replace key and value
+    elsif winner[:number] > number    
       winner[:name] = name  
       winner[:number] = number
     end
   end
   winner[:name]   #=> key `:name` of winner
 end
-
